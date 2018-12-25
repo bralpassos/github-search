913cf9364d7d71b1c4695ffee15ce033b3a5385a
Bruno Passos <bruno.passos@lambda3.com.br>
2018-12-25 00:36:51 -0200

    Default search page with initial layout

---
+++ src/app/search/search.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-search',
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.scss']
+})
+export class SearchComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  searchByUsername() {
+    
+  }
+
+}
